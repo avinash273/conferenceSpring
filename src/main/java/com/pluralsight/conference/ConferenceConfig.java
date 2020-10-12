4e52c5fc75ae83fd22a329728650332e09107a94
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-12 12:52:26 -0500

    Language internationalizing using Interceptors

--- src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -2,10 +2,15 @@ package com.pluralsight.conference;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 /**
  * This part removed from application.properties
@@ -20,6 +25,20 @@ public class ConferenceConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("/WEB-INF/pdf/");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 
     @Bean
